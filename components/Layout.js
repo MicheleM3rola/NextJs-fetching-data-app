d1c7cffaa7125409ffb832b08e1820eb7518d78b
Mike <mike88.merola@gmail.com>
2021-02-16 22:21:37 +0800

    second next project up

---
+++ components/Layout.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "../styles/Layout.module.css";
+import Header from "./Header";
+import Nav from "./Nav";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Nav />
+      <div className={styles.container}>
+        <Header />
+        <main className={styles.main}>{children}</main>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
---
+++ pages/about.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const about = () => {
+  return <div>Hello I am about</div>;
+};
+
+export default about;
